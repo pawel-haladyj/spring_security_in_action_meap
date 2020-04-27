a80b3a0e5cfa046b1bbfdf505f9a418603c33f22
Pawel Haladyj <pawel@haladyj.pl>
2020-04-27 07:39:24 +0200

    CH6 init

---
+++ SpringSecurity_configuring_authorization_CH6/src/main/java/pl/haladyj/SpringSecurity_configuring_authorization_CH6/SpringSecurityConfiguringAuthorizationCh6Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_configuring_authorization_CH6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityConfiguringAuthorizationCh6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityConfiguringAuthorizationCh6Application.class, args);
+	}
+
+}
---
+++ SpringSecurity_configuring_authorization_CH6/src/test/java/pl/haladyj/SpringSecurity_configuring_authorization_CH6/SpringSecurityConfiguringAuthorizationCh6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_configuring_authorization_CH6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityConfiguringAuthorizationCh6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
