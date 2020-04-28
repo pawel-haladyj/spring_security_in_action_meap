90e62bea7e670e41a6019870d8f6a85dd0c7f20e
Paweł Haładyj <pawel@haladyj.pl>
2020-04-28 09:00:19 +0200

    implemented passwordEncoder

--- SpringSecurity_Hello_Spring_Security_CH2/src/main/java/pl/haladyj/SpringSecurity_Hello_Spring_Security_CH2/configuration/ProjectConfiguration.java
+++ SpringSecurity_Hello_Spring_Security_CH2/src/main/java/pl/haladyj/SpringSecurity_Hello_Spring_Security_CH2/configuration/ProjectConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -22,6 +24,16 @@ public class ProjectConfiguration {
         userDetailsService.createUser(user);
 
         return userDetailsService;
+    }
+
+    /**
+     * used NoOpPasswordEncoder, change an instance in case of implementing to production
+     * @return
+     */
+    @Bean
+    @Deprecated
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
