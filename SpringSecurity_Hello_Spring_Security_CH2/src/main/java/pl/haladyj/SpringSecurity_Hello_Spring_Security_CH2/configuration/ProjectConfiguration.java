b3df8940b289b449e7048947edaac78a7e0e6f1a
Paweł Haładyj <pawel@haladyj.pl>
2020-04-28 09:31:13 +0200

    auth configuration with WSCA configure(auth)

--- SpringSecurity_Hello_Spring_Security_CH2/src/main/java/pl/haladyj/SpringSecurity_Hello_Spring_Security_CH2/configuration/ProjectConfiguration.java
+++ SpringSecurity_Hello_Spring_Security_CH2/src/main/java/pl/haladyj/SpringSecurity_Hello_Spring_Security_CH2/configuration/ProjectConfiguration.java
@@ -2,6 +2,7 @@ package pl.haladyj.SpringSecurity_Hello_Spring_Security_CH2.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,7 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
+/*    @Bean
     public UserDetailsService userDetailsService(){
         var userDetailsService = new InMemoryUserDetailsManager();
 
@@ -28,21 +29,45 @@ public class ProjectConfiguration extends WebSecurityConfigurerAdapter {
         return userDetailsService;
     }
 
-    /**
+    *//**
      * used NoOpPasswordEncoder, change an instance in case of implementing to production
      * @return
-     */
+     *//*
     @Bean
     @Deprecated
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests()
                 .anyRequest()
-                .authenticated();
+                .permitAll();
+    }
+
+
+    /**
+     * CAUTION: used NoOpPasswordEncoder - do not use in production
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    @Deprecated
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User
+                .withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
