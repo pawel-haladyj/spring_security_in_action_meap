06e77d5295253ecaca5a19aae13f00a4e8ccdf3f
Paweł Haładyj <pawel@haladyj.pl>
2020-04-28 11:55:58 +0200

    CH3 init

---
+++ SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/SpringSecurityManagingUsersAndPasswordsCh3Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_Managing_Users_And_Passwords_CH3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityManagingUsersAndPasswordsCh3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityManagingUsersAndPasswordsCh3Application.class, args);
+	}
+
+}
---
+++ SpringSecurity_Managing_Users_And_Passwords_CH3/src/test/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/SpringSecurityManagingUsersAndPasswordsCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_Managing_Users_And_Passwords_CH3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityManagingUsersAndPasswordsCh3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
