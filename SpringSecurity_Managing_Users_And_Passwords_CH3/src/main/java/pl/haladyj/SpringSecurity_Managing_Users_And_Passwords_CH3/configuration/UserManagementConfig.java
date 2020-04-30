05520b6187dbeb4ec02d2757567c37eda63290db
Paweł Haładyj <pawel@haladyj.pl>
2020-04-30 11:49:03 +0200

    LDAP?

--- SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/configuration/UserManagementConfig.java
+++ SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/configuration/UserManagementConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -13,14 +16,31 @@ import javax.sql.DataSource;
 @Configuration
 public class UserManagementConfig {
 
-    @Bean
+/*    @Bean
     public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
 
+        String usersByUsernameQuery = "SELECT username, password, enabled FROM users WHERE username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+
+        return userDetailsManager;
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var cs = new DefaultSpringSecurityContextSource("ldap://127.0.0.1:33389/dc=springframework,dc=org");
+        cs.afterPropertiesSet();
+
+        LdapUserDetailsManager manager = new LdapUserDetailsManager(cs);
+        manager.setUsernameMapper(new DefaultLdapUsernameToDnMapper("ou=user", "cn"));
+        manager.setGroupSearchBase("ou=groups");
+
+        return manager;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
