50a68b5abe93d8d52b75c37eb666b69e5132f03e
Paweł Haładyj <pawel@haladyj.pl>
2020-04-30 08:51:00 +0200

    SimpleUser

--- SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/model/DummyUser.java
+++ SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/model/DummyUser.java
@@ -4,12 +4,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class DummyUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(()->"READ");
     }
 
     @Override
