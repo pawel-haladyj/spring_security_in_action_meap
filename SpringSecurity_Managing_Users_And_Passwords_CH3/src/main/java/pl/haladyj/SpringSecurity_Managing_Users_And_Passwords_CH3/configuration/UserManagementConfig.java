c73f639a59b15cbd9b916130d2da84081874afa4
Paweł Haładyj <pawel@haladyj.pl>
2020-04-30 10:09:15 +0200

    creating User.UserBuilder instance

--- SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/configuration/UserManagementConfig.java
+++ SpringSecurity_Managing_Users_And_Passwords_CH3/src/main/java/pl/haladyj/SpringSecurity_Managing_Users_And_Passwords_CH3/configuration/UserManagementConfig.java
@@ -5,17 +5,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 public class UserManagementConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails u = User
+/*        UserDetails u = User
                 .withUsername("bill")
                 .authorities("read","write")
                 .accountExpired(false)
                 .disabled(true)
-                .build();
+                .build();*/
+
+            User.UserBuilder builder1 = User.withUsername("bill");
+
+            UserDetails u = builder1
+                    .password("12345")
+                    .authorities("read","write")
+                    .accountExpired(false)
+                    .disabled(true)
+                    .build();
+
+            User.UserBuilder builder2 = User.withUserDetails(u);
+
+            return (UserDetailsService) builder2;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
