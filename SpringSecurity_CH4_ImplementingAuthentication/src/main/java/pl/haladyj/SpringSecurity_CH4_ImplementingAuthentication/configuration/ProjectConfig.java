abe60a1ca32b7c680692ea7ba27341f4d9bb8ba6
Paweł Haładyj <pawel@haladyj.pl>
2020-05-05 12:31:28 +0200

    customizing failed anser with httpBasic (curl -v uri)

--- SpringSecurity_CH4_ImplementingAuthentication/src/main/java/pl/haladyj/SpringSecurity_CH4_ImplementingAuthentication/configuration/ProjectConfig.java
+++ SpringSecurity_CH4_ImplementingAuthentication/src/main/java/pl/haladyj/SpringSecurity_CH4_ImplementingAuthentication/configuration/ProjectConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -49,5 +50,14 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(customAuthenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(c->{
+            c.realmName("OTHER");
+        });
+
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
