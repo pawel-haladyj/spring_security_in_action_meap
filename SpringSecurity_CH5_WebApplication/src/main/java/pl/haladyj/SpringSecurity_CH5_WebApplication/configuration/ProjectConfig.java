ff72ea44dd255a2c89bb13441074539d6797dc30
Paweł Haładyj <pawel@haladyj.pl>
2020-05-06 09:43:39 +0200

    passwordEncoders @Config

---
+++ SpringSecurity_CH5_WebApplication/src/main/java/pl/haladyj/SpringSecurity_CH5_WebApplication/configuration/ProjectConfig.java
@@ -0,0 +1,21 @@
+package pl.haladyj.SpringSecurity_CH5_WebApplication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder(){
+        return new SCryptPasswordEncoder();
+    }
+
+}
