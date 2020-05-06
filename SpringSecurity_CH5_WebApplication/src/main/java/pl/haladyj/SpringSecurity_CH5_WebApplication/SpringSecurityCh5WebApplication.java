ed9708263fd540af68e16e2598f1e033c0da9461
Paweł Haładyj <pawel@haladyj.pl>
2020-05-06 07:39:52 +0200

    CH5 init

---
+++ SpringSecurity_CH5_WebApplication/src/main/java/pl/haladyj/SpringSecurity_CH5_WebApplication/SpringSecurityCh5WebApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_CH5_WebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCh5WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCh5WebApplication.class, args);
+	}
+
+}
---
+++ SpringSecurity_CH5_WebApplication/src/test/java/pl/haladyj/SpringSecurity_CH5_WebApplication/SpringSecurityCh5WebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_CH5_WebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCh5WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
