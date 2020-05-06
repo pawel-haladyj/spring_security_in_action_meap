7ec18c2c6751d083b7cda92b58d48cf79d3737d4
Paweł Haładyj <pawel@haladyj.pl>
2020-05-06 11:32:30 +0200

    initialization data.sql

---
+++ SpringSecurity_CH5_WebApplication/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO users (id, username, password, algorithm) VALUES ('1', 'john', '$2a$10$xn3LI/AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT');
+
+INSERT IGNORE INTO authority (id, name, user) VALUES ('1', 'READ', '1'), ('2', 'WRITE', '1');
+
+INSERT IGNORE INTO product (id, name, price, currency) VALUES ('1', 'Chocolate', '10', 'USD');
