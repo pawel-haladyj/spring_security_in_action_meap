1994d4f60ab28fbfcfab89bce5b9fb69783eca60
Paweł Haładyj <pawel@haladyj.pl>
2020-05-07 12:32:35 +0200

    CH7 init

---
+++ SpringSecurity_CH7_Implementing_Filters/src/main/java/pl/haladyj/SpringSecurity_CH7_Implementing_Filters/SpringSecurityCh7ImplementingFiltersApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_CH7_Implementing_Filters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCh7ImplementingFiltersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCh7ImplementingFiltersApplication.class, args);
+	}
+
+}
---
+++ SpringSecurity_CH7_Implementing_Filters/src/test/java/pl/haladyj/SpringSecurity_CH7_Implementing_Filters/SpringSecurityCh7ImplementingFiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.SpringSecurity_CH7_Implementing_Filters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCh7ImplementingFiltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
